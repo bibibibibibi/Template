b44cf6fe6f8d993ffbedb8d65f66c879885bbd78
bibibibibibi <623418780@qq.com>
2019-02-15 22:49:48 +0800

    upd

--- 数论/快速求欧拉函数.cpp
+++ 数论/快速求欧拉函数.cpp
@@ -11,17 +11,15 @@ void init()
 				phi[j]=(phi[j]/i)*(i-1); //j有因子i,而且i是素数,正是欧拉函数
 }
 
+//利用积性函数性质，分解素因子之后进行求解
 ll phi(ll n)
 {
 	ll ret=n;
 	for(ll i=2;i*i<=n;i++)
 	{
-		if(n%i==0)
-			ret=ret/i*(i-1);
-		while(n%i==0)
-			n/=i;
+		if(n%i==0) ret=ret/i*(i-1);
+		while(n%i==0) n/=i;
 	}
-	if(n>1)
-		ret=ret/n*(n-1);
+	if(n>1) ret=ret/n*(n-1);
 	return ret;
 }
