c76427ef21f690dbb2a2915c856a1def649d4f63
Liu Yang <623418780@qq.com>
2019-05-16 22:08:44 +0800

    pd

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define xx first
+#define yy second
+#define mp make_pair
+#define pb push_back
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main()
+{
+	//freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
+	
+	return 0;
+}
