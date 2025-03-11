2137bc19dab06ccc8ef305dca461b41bda92799d
Liu Yang <623418780@qq.com>
2025-03-11 15:02:22 +0800

    Update 带撤销并查集.cpp

--- 数据结构/带撤销并查集.cpp
+++ 数据结构/带撤销并查集.cpp
@@ -1,39 +1,42 @@
 struct UFS
 {
-	stack<pair<int*, int>> stk;
-	int fa[MAXN],rnk[MAXN];
-	inline void init(int n)
+	
+	stack<pair<int, int> > stk;
+	int fa[MAXN],sz[MAXN];
+	void init(int n)
 	{
-		for(int i=0;i<=n;i++) fa[i]=i,rnk[i]=0;
+		for(int i=0;i<=n;i++) fa[i]=i,sz[i]=1;
 	}
-	inline int Find(int x)
+	int Find(int x)
 	{
-		while(x^fa[x]) x=fa[x];
+		while(x!=fa[x]) x=fa[x];
 		return x;
 	}
-	inline void Merge(int x,int y)
+	void merge(int x,int y)
 	{
 		x=Find(x),y=Find(y);
 		if(x==y) return ;
-		if(rnk[x]<=rnk[y])
-		{
-			stk.push({fa+x,fa[x]});
-			fa[x]=y;
-			if(rnk[x]==rnk[y])
-			{
-				stk.push({rnk+y,rnk[y]});
-				rnk[y]++;
-			}
-		}
-		else
-		{
-			stk.push({fa+y,fa[y]});
-			fa[y]=x;
-		}
+		if(sz[x]<sz[y]) swap(x,y);
+		stk.push({y,fa[y]});
+		fa[y]=x;
+		sz[x]+=sz[y];
 	}
-	inline void Undo()
+	void undo(int x)
 	{
-		*stk.top().xx=stk.top().yy;
-		stk.pop();
+		while(stk.size()>x)
+		{
+			fa[stk.top().first]=stk.top().second;
+			sz[stk.top().second]-=sz[stk.top().first];
+			stk.pop();
+		}
+		
+	}
+	int size(int x)
+	{
+		return sz[Find(x)];
+	}
+	int history()
+	{
+		return stk.size();
 	}
 };
