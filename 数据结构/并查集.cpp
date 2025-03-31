47af4695d343f10c6b0f68977963b0cf373c0a8d
Liu Yang <623418780@qq.com>
2025-03-31 22:43:52 +0800

    Update 并查集.cpp

--- 数据结构/并查集.cpp
+++ 数据结构/并查集.cpp
@@ -1,45 +1,4 @@
-int par[MAXN],Rank[MAXN];
-
-void init(int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        par[i]=i;
-        Rank[i]=0;
-    }
-}
-
-int find(int x)
-{
-    if(par[x]==x)
-        return x;
-    else
-        return par[x]=find(par[x]);
-}
-
-void unite(int x,int y)
-{
-    x=find(x);
-    y=find(y);
-    if(x==y)
-        return;
-    if(Rank[x]<Rank[y])
-        par[x]=y;
-    else
-    {
-        par[y]=x;
-        if(Rank[x]==Rank[y])
-            Rank[x]++;
-    } 
-}
-
-bool same(int x,int y)
-{
-    return find(x)==find(y);
-}
-
-//Q
-namespace DSU
+struct DSU
 {
     int fa[MAXN];
     void init(int n)
@@ -56,6 +15,4 @@ namespace DSU
         if(x==y) return 0;
         return fa[x]=y,1;
     }
-}
-
-
+};
