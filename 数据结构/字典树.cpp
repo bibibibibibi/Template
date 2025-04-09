62247b08afbdf1ead5f2f8598f871359c7d4f01a
Liu Yang <623418780@qq.com>
2025-04-09 11:21:37 +0800

    Update 字典树.cpp

--- 数据结构/字典树.cpp
+++ 数据结构/字典树.cpp
@@ -1,56 +1,39 @@
-typedef long long ll;
 struct Tri
 {
-    int ch[MAXN][2];
-    int sz[MAXN][2];
+    int ch[MAXN*32][2];
+	int cnt[MAXN*32];
     int tot;
     void init()
     {
         memset(ch,0,sizeof(ch));
-        memset(sz,0,sizeof(sz));
+		memset(cnt,0,sizeof(cnt));
         tot=2;
     }
-    void insert(int x)
+    void add(int x,int v)
     {
         int u=1;
         for(int i=30;i>=0;i--)
         {
             int p=(x>>i)&1;
             if(!ch[u][p])ch[u][p]=tot++;
-            sz[u][p]++;
             u=ch[u][p];
+			cnt[u]+=v;
         }
     }
-    void del(int x)
-    {
-        int p;
-        int u=1;
-        for(int i=30;i>=0;i--)
-        {
-            p=(x>>i)&1;
-            sz[u][p]--;
-            if(sz[u][p]==0)
-                ch[u][p]=0;
-            u=ch[u][p];
-        }
-    }
-    ll get(int x)
+	int get(int x)
     {
         int u=1;
-        ll ans=0;
+        int ans=0;
         for(int i=30;i>=0;i--)
         {
             int p=(x>>i)&1;
-            if(ch[u][p^1])
+            if(cnt[ch[u][p^1]])
             {
+				p^=1;
                 ans^=1<<i;
-                u=ch[u][p^1];
             }
-            else
-            {
-                u=ch[u][p];
-            }
+			u=ch[u][p];
         }
         return ans;
     }
-}T;
+}trie;
